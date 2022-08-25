959a32de9e81bdc7f4afc06b3e261a61a958065e
ASTR001 <astrsmartcity@gmail.com>
2022-08-25 12:50:26 +0530

    new push

---
+++ android/app/src/main/java/com/gomeat/app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.neo.allycart;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=F:\FluterSdk\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\NeophoronTech\Neo GitHub Projects\allycart_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.18"
+export "FLUTTER_BUILD_NUMBER=19"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
